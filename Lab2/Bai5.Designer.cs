a3eda7df6a20eb178bdb920fad59df7ca1546f0b
Khoadnd <20521463@ms.uit.edu.vn>
2022-04-13 14:28:38 +0700

    Added Bai 5

--- Lab2/Bai5.Designer.cs
+++ Lab2/Bai5.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab2
 {
-    partial class Form1
+    partial class Bai5
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bai5));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tvTree = new System.Windows.Forms.TreeView();
+            this.lsvDetails = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvTree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lsvDetails);
+            this.splitContainer1.Size = new System.Drawing.Size(593, 313);
+            this.splitContainer1.SplitterDistance = 197;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Paomedia-Small-N-Flat-Folder.ico");
+            this.imageList1.Images.SetKeyName(1, "Oxygen-Icons.org-Oxygen-Actions-document-edit.ico");
+            // 
+            // tvTree
+            // 
+            this.tvTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvTree.ImageIndex = 0;
+            this.tvTree.ImageList = this.imageList1;
+            this.tvTree.Location = new System.Drawing.Point(0, 0);
+            this.tvTree.Name = "tvTree";
+            this.tvTree.SelectedImageIndex = 0;
+            this.tvTree.Size = new System.Drawing.Size(197, 313);
+            this.tvTree.TabIndex = 1;
+            this.tvTree.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvTree_NodeMouseClick);
+            // 
+            // lsvDetails
+            // 
+            this.lsvDetails.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4,
+            this.columnHeader3});
+            this.lsvDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvDetails.HideSelection = false;
+            this.lsvDetails.Location = new System.Drawing.Point(0, 0);
+            this.lsvDetails.Name = "lsvDetails";
+            this.lsvDetails.Size = new System.Drawing.Size(392, 313);
+            this.lsvDetails.SmallImageList = this.imageList1;
+            this.lsvDetails.TabIndex = 0;
+            this.lsvDetails.UseCompatibleStateImageBehavior = false;
+            this.lsvDetails.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tên file";
+            this.columnHeader1.Width = 98;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kích thước";
+            this.columnHeader2.Width = 67;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Đuôi mở rộng";
+            this.columnHeader4.Width = 75;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ngày tạo";
+            this.columnHeader3.Width = 187;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(593, 313);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BÀI 5";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvTree;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView lsvDetails;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
--- Lab2/Bai5.cs
+++ Lab2/Bai5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,83 @@ using System.Windows.Forms;
 
 namespace Lab2
 {
-    public partial class Form1 : Form
+    public partial class Bai5 : Form
     {
-        public Form1()
+        public Bai5()
         {
             InitializeComponent();
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+
+            DirectoryInfo info = new DirectoryInfo(@"../..");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                tvTree.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs,
+            TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        void tvTree_NodeMouseClick(object sender,
+            TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            lsvDetails.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, ""),
+                    new ListViewItem.ListViewSubItem(item, "Folder"),
+                    new ListViewItem.ListViewSubItem(item, dir.CreationTime.ToString())
+                };
+                item.SubItems.AddRange(subItems);
+                lsvDetails.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                { 
+                    new ListViewItem.ListViewSubItem(item, file.Length.ToString()),
+                    new ListViewItem.ListViewSubItem(item, file.Extension),
+                    new ListViewItem.ListViewSubItem(item, file.CreationTime.ToString())
+                };
+
+                item.SubItems.AddRange(subItems);
+                lsvDetails.Items.Add(item);
+            }
+
+            lsvDetails.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
         }
     }
 }
